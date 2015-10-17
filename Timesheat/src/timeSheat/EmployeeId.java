6ea4477ebd9f8c461f11b467c2339a60b812fb19
halver <spha4es9@watch.ocn.ne.jp>
2015-10-17 11:24:58 +0900

    StuffId.javaをemployee.javaに変更

---
+++ Timesheat/src/timeSheat/EmployeeId.java
@@ -0,0 +1,23 @@
+package timeSheat;
+
+import java.util.InputMismatchException;
+
+public class EmployeeId {
+	@SuppressWarnings("resource")
+	public static int employeeId(){
+		System.out.println("社員番号を入力してください");
+		Integer si;
+		for(;;){
+			try{
+				si = new java.util.Scanner(System.in).nextInt();
+				while( Integer.toString(si).length()>6){
+					System.out.println("社員番号が長過ぎます。6桁までの数字で入力してください");
+					si = new java.util.Scanner(System.in).nextInt();
+				}
+				return si;
+			}catch(InputMismatchException e){
+				System.out.println("社員番号は6桁の数字で入力してください。");
+			}
+		}
+	}
+}
