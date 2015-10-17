2ec989ed0b2d021d14c7c02525a0fc42317c6d9e
halver <spha4es9@watch.ocn.ne.jp>
2015-10-17 14:57:18 +0900

    操作を選択できるよう分岐を追加

--- Timesheat/src/timeSheat/TimeSheat.java
+++ Timesheat/src/timeSheat/TimeSheat.java
@@ -4,8 +4,26 @@ import java.io.IOException;
 
 public class TimeSheat {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
-		inputdb in = new inputdb();
-		in.input();
+		System.out.println("操作を選択してください。");
+		System.out.println("1:追加　2:更新　3:削除　4:確認　5:終了");
+		int n = new java.util.Scanner(System.in).nextInt();
+		for(;;) {
+			if(n==1){
+				inputdb in = new inputdb();
+				in.input();
+				System.out.println("操作を選択してください。");
+				System.out.println("1:追加　2:更新　3:削除　4:確認　5:終了");
+				n = new java.util.Scanner(System.in).nextInt();
+			}else if (n==5){
+				System.out.println("操作を終了します。");
+				break;
+			}else{
+				System.out.println("操作を選択してください。");
+				System.out.println("1:追加　2:更新　3:削除　4:確認　5:終了");
+				n = new java.util.Scanner(System.in).nextInt();
+			}
+		}
 	}
 }
