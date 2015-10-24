30136f1d7e4d23a2bc646dedc98c8e4e15a3a524
halver <spha4es9@watch.ocn.ne.jp>
2015-10-24 14:27:17 +0900

    分岐をswitch文に書き換え

--- Timesheat/src/timeSheat/TimeSheat.java
+++ Timesheat/src/timeSheat/TimeSheat.java
@@ -8,39 +8,29 @@ public class TimeSheat {
 		System.out.println("操作を選択してください。");
 		System.out.println("1:追加　2:更新　3:削除　4:確認　5:終了");
 		int n = new java.util.Scanner(System.in).nextInt();
-		for(;;) {
-			if(n==1){
+		while(n!=5){
+			switch(n) {
+			case 1:
 				InputDb in = new InputDb();
 				in.input();
-				System.out.println("操作を選択してください。");
-				System.out.println("1:追加　2:更新　3:削除　4:確認　5:終了");
-				n = new java.util.Scanner(System.in).nextInt();
-			}else if (n==2){
+				break;
+			case 2:
 				UpdataDb up = new UpdataDb();
 				up.updata();
-				System.out.println("操作を選択してください。");
-				System.out.println("1:追加　2:更新　3:削除　4:確認　5:終了");
-				n = new java.util.Scanner(System.in).nextInt();
-			}else if (n==3){
-				DeleatDb up = new DeleatDb();
-				up.deleat();
-				System.out.println("操作を選択してください。");
-				System.out.println("1:追加　2:更新　3:削除　4:確認　5:終了");
-				n = new java.util.Scanner(System.in).nextInt();
-			}else if (n==4){
+				break;
+			case 3:
+				DeleatDb del = new DeleatDb();
+				del.deleat();
+				break;
+			case 4:
 				ConfirmationDb con = new ConfirmationDb();
 				con.confirmation();
-				System.out.println("操作を選択してください。");
-				System.out.println("1:追加　2:更新　3:削除　4:確認　5:終了");
-				n = new java.util.Scanner(System.in).nextInt();
-			}else if (n==5){
-				System.out.println("操作を終了します。");
 				break;
-			}else{
-				System.out.println("操作を選択してください。");
-				System.out.println("1:追加　2:更新　3:削除　4:確認　5:終了");
-				n = new java.util.Scanner(System.in).nextInt();
 			}
+			System.out.println("操作を選択してください。");
+			System.out.println("1:追加　2:更新　3:削除　4:確認　5:終了");
+			n = new java.util.Scanner(System.in).nextInt();
 		}
+		System.out.println("操作を終了します。");
 	}
 }
